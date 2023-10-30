e601c63ebc476c789ba66300d7fbd29964778fc1
shehriar <74128874+shehriar@users.noreply.github.com>
2023-10-30 13:22:55 -0500

    Add files via upload

---
+++ code/Restaurants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  @override
+  _RestaurantScreen createState() => _RestaurantScreen();
+}
+
+class _RestaurantScreen extends State<RestaurantScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
