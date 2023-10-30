414735a0a95d52c4639cd857b761a61b321ec76f
shehriar <74128874+shehriar@users.noreply.github.com>
2023-10-30 13:21:59 -0500

    Create main.dart

---
+++ code/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shehriar_burney_mp2/Restaurants.dart';
+import 'package:shehriar_burney_mp2/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Explore Chicago',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
